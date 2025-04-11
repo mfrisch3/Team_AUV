76d5014c7116b7ec7c5245342f85d514d14284e8
Declan Stanton <declan.m.stanton@gmail.com>
2025-04-11 16:15:51 -0500

    IMU code for i2c

---
+++ sensor_code/IMU_firmware.cpp
@@ -0,0 +1,223 @@
+#include <Wire.h>
+
+//  board addresses for I2C setup
+#define ICM_READ  0xD5
+#define ICM_WRITE 0xD4
+
+// IMU i2c address
+#define IMU_ADDR  0x6A
+
+//  register addresses for accelerometer
+#define OUTZ_L_XL 0x2C
+#define WHO_AM_I  0x0F
+#define OUTZ_H_XL 0x2D
+#define X_XL_ADDR 0X28
+#define Y_XL_ADDR 0x2A
+
+//  register addresses for gyroscope
+#define OUTX_L_G 0x22
+#define OUTX_H_G 0x23
+#define OUTY_L_G 0x24
+#define OUTY_H_G 0x25
+#define OUTZ_L_G 0x26
+#define OUTZ_H_G 0x27
+
+
+// register data for accelerometer & gyro setup
+#define XL_CNTRL  0x10
+#define G_CNTRL   0x11
+#define LOW_POWER 0b00110000
+#define HIGH_PFMNC 0b10100000
+
+// setup i2c pins on arduino
+#define SDA_PIN A4
+#define SCL_PIN A5
+
+//Global Variables
+byte RxBuffer[100] = {0};
+int RxIdx = 0;
+const float GYRO_MULT = 0.004375;
+
+struct IMUData {
+  int16_t ax, ay, az;
+  int16_t gx, gy, gz;
+};
+
+bool check_IMU() {
+  uint8_t error;
+  Wire.beginTransmission(IMU_ADDR);
+  error = Wire.endTransmission();
+
+  if(error == 0){
+    Serial.print("Device found at address: 0x");
+    Serial.println(IMU_ADDR, HEX);
+    return true;
+  }
+  Serial.print("Board not found");
+  return false;
+}
+
+bool read_test(){
+  uint8_t error;
+  uint8_t val;
+
+  // se
+  Wire.beginTransmission(IMU_ADDR);
+  Wire.write(WHO_AM_I);
+  error = Wire.endTransmission();
+
+  // error from transmission
+  if(error != 0){
+    Serial.print("Error connecting to device w/ error: ");
+    Serial.println(error);
+  }
+
+  Wire.requestFrom(IMU_ADDR, 1);
+
+  if(Wire.available() == 1){
+    Serial.print("WHO_AM_I: ");
+    Serial.println(Wire.read(), HEX);
+    return 1;
+  }
+
+  return 0;
+}
+
+void get_events(IMUData* data){
+  Wire.beginTransmission(IMU_ADDR);
+  Wire.write(OUTX_L_G);
+  Wire.endTransmission(false);
+  Wire.requestFrom(IMU_ADDR, 12); // 12 bytes, 2 bytes for each x,y,z of accel and gyro
+
+  uint8_t buffer[12];
+  for (int i = 0; i < 12; i++) buffer[i] = Wire.read();
+
+  data->gx = (buffer[1] << 8) | buffer[0];
+  data->gy = (buffer[3] << 8) | buffer[2];
+  data->gz = (buffer[5] << 8) | buffer[4];
+  data->ax = (buffer[7] << 8) | buffer[6];
+  data->ay = (buffer[9] << 8) | buffer[8];
+  data->az = (buffer[11] << 8) | buffer[10];
+}
+
+// global variables
+double height;
+double velocity = 0;
+unsigned long prevTime = 0;
+double a_ema = 0;
+double pitch = 0;
+double yaw = 0;
+double roll = 0;
+IMUData events;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Begin I2C Connection");
+  Wire.begin();
+
+  if(!check_IMU()){
+    Serial.println("Search for IMU failed");
+  }
+
+  if(read_test()){
+    Serial.println("IMU I2C protocol success");
+  }
+
+  // let's actually turn on the accelerometer this time lol
+  Wire.beginTransmission(IMU_ADDR);
+  Wire.write(XL_CNTRL);
+  // Wire.write(LOW_POWER); //low power but 52Hz sampling
+  Wire.write(HIGH_PFMNC); //high power but 6.66kHz sampling
+  Wire.endTransmission();
+
+  Wire.beginTransmission(IMU_ADDR);
+  Wire.write(G_CNTRL);
+  // Wire.write(LOW_POWER); //low power but 52Hz sampling
+  Wire.write(HIGH_PFMNC); //high power but 6.66kHz sampling
+  Wire.endTransmission();
+
+  Wire.requestFrom(IMU_ADDR, 1);
+
+  prevTime = micros();
+}
+
+void loop() {
+  // Get current time
+  unsigned long currentTime = micros();
+  double dt = (currentTime - prevTime) / 1e6; // in seconds
+  prevTime = currentTime;
+
+  get_events(&events);
+  // Get raw Z-acceleration
+  int16_t z_raw = events.az;
+
+  // Convert raw to m/s² (assuming ±2g FS range)
+  double a = z_raw * 0.061 * 9.80665 / 1000.0 - 9.80665;
+  double gx = events.gx * GYRO_MULT;
+  double gy = events.gy * GYRO_MULT;
+  double gz = events.gz * GYRO_MULT;
+
+  // EMA filter
+  const float alpha = 0.85;
+  a_ema = alpha * a_ema + (1-alpha) * a;
+  a = a_ema;
+
+  // ignore xl noise
+  if (abs(a) < 0.2){
+    // zero velocity correction
+    if (abs(a) < 0.03){
+      velocity = 0;
+    }
+    a = 0;
+  } 
+
+  // ignore gyro_noise
+  if (abs(gx) < 0.1){
+    gx = 0;
+  }
+
+  if (abs(gy) < 0.1){
+    gy = 0;
+  }
+
+  if (abs(gz) < 0.1){
+    gz = 0;
+  }
+
+  //  Integrate gyroscope data
+  pitch += gx * dt;
+  yaw += gy * dt;
+  roll += gz * dt;
+
+  // Integrate acceleration to get velocity
+  velocity += a * dt;
+
+  // Integrate velocity to get height
+  height += velocity * dt;
+
+  // Output
+  Serial.print("Const:");
+  Serial.println(1);
+  Serial.print(",");
+
+  // // Gyroscope Outputs
+  Serial.print("Pitch:");
+  Serial.println(pitch, 4);
+  Serial.println(",");
+  Serial.print("Yaw:");
+  Serial.println(yaw, 4);
+  Serial.println(",");
+  Serial.print("Roll:");
+  Serial.println(roll, 4);
+  // // Acceleration and Height
+
+  // Serial.print("Accel:");
+  // Serial.println(a, 4);
+  // Serial.print(",");
+  // Serial.print("Velocity:");
+  // Serial.println(velocity, 4);
+  // Serial.print(",");
+  // Serial.print("Height:");
+  // Serial.println(height, 6);
+}
